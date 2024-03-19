71989047e8f2e32b4bf393900e794bd4453c8266
amine <amineelkhayyar8@gmail.com>
2024-03-19 18:44:00 +0100

    19 - 03 - 2024
    Clase de Registrar , donde se implemnta la interfaz de la pantalla de registro
    
    No tiene nada en el main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.rollerpizza_tfg"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.rollerpizza_tfg"
