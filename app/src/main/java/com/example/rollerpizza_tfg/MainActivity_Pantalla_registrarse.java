7c97c25077b4a2391fd35dc69ed142a3e6e679a2
kevin <kevin28042001@gmail.com>
2024-03-20 00:01:09 +0100

    Pantallas de Transiciones (pasa del 1 al 2, pero hasta ahi, falta completar del 2 al 3) Pantalla Inicio funcional, nos mueve a inicio de sesion y Registrarse Inicio de sesion permite moverse a Registrarse y Viceversa
    Visca Barça, Psoe cabron

--- app/src/main/java/com/example/rollerpizza_tfg/MainActivity_Pantalla_registrarse.java
+++ app/src/main/java/com/example/rollerpizza_tfg/MainActivity_Pantalla_registrarse.java
@@ -3,7 +3,10 @@ package com.example.rollerpizza_tfg;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity_Pantalla_registrarse extends AppCompatActivity {
 
@@ -12,6 +15,16 @@ public class MainActivity_Pantalla_registrarse extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_pantalla_registrarse);
         getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity_Pantalla_registrarse.this,R.color.rojo));
+        TextView inicia_sesion=findViewById(R.id.textView_iniciasesion);
+
+        inicia_sesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Aquí es donde se inicia el nuevo Activity
+                Intent intent = new Intent(MainActivity_Pantalla_registrarse.this, MainActivity_IniciarSesion.class);
+                startActivity(intent);
+            }
+        });
 
         
 
