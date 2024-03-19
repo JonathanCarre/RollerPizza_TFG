aa412256ebdce1e6858123a22a94cae5469dbfba
Jony <carrenojonathan85@gmail.com>
2024-03-19 23:06:00 +0100

    Commit He unido la pantalla inicial con la de registro del moha

--- app/src/main/java/com/example/rollerpizza_tfg/MainActivity_PantallaInicio.java
+++ app/src/main/java/com/example/rollerpizza_tfg/MainActivity_PantallaInicio.java
@@ -1,9 +1,17 @@
 package com.example.rollerpizza_tfg;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity_PantallaInicio extends AppCompatActivity {
 
@@ -12,5 +20,13 @@ public class MainActivity_PantallaInicio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_pantalla_inicio);
         getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity_PantallaInicio.this,R.color.mi_color_de_barra_de_notificaciones));
+        Button pasar_activity_pantalla_registro=findViewById(R.id.boton_registrarse);
+        pasar_activity_pantalla_registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity_PantallaInicio.this, MainActivity_Pantalla_registrarse.class);
+                startActivity(intent);
+            }
+        });
     }
 }
