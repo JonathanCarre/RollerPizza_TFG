698d1f2adbae46a37314b1e677bfed7d4237cfef
amine <amineelkhayyar8@gmail.com>
2024-03-22 19:15:51 +0100

    22 - 03 - 2024
    Pantalla de registro:
    
     Se ha puesto un calendario inttuitivo para el ususario

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,4 +36,12 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Dependecia de CALENDARIO (CLASE PANTALLA REGISTRARSE)
+    implementation ("com.google.android.material:material:1.5.0-alpha04")
+
+
+
+
+
 }
